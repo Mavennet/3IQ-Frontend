5019ac8e8b7cd2d1ab276166d234c65734d5bd9c
Ian Silva <ian.silva@verity.com.br>
2022-09-05 14:23:39 -0300

    change none to transprent

--- web/components/Header/NavItem/NavItem.js
+++ web/components/Header/NavItem/NavItem.js
@@ -18,7 +18,7 @@ function NavItem(props) {
           width: {xs: '100%', md: 'auto'},
           marginLeft: {xs: '0', md: '30px'},
           padding: {xs: '12px 16px', md: '0px'},
-          backgroundColor: {xs: '#fbfbfb', md: 'none'},
+          backgroundColor: {xs: '#fbfbfb', md: 'transparent'},
           color: '#000',
           textTransform: 'capitalize',
           fontSize: '18px',
