1954d2bcd10214bdff8f3fba4d8a16a423c71ad2
Ian Silva <ian.silva@verity.com.br>
2022-09-05 13:24:53 -0300

    change shared module

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import BaseApp from 'next/app'
 import client from '../client'
-//import '../styles/shared.module.css'
+import '../styles/shared.module.css'
 import '../styles/layout.css'
 import '../styles/custom-properties.css'
 
