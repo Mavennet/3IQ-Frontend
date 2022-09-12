b840c2664eff95e495da374e7b7794df4218f838
Ian Silva <ian.silva@verity.com.br>
2022-09-12 17:27:18 -0300

    [WEB-86] create text separator section

--- web/components/sections/TextSeparator/TextSeparator.js
+++ web/components/sections/TextSeparator/TextSeparator.js
@@ -1,46 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {createTheme, ThemeProvider} from '@mui/material/styles'
-// import imageUrlBuilder from '@sanity/image-url'
-// import client from '../../../client'
-// import CssBaseline from '@mui/material/CssBaseline'
-// import Box from '@mui/material/Box'
-// import Typography from '@mui/material/Typography'
-// import Grid from '@mui/material/Grid'
-// import RedirectButton from '../../RedirectButton/RedirectButton'
-// import {format, parseISO} from 'date-fns'
-// import SimpleBlockContent from '../../SimpleBlockContent'
-// import styles from './TextSeparator.module.css'
-
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Europa',
-    p: {
-      fontSize: 14,
-    },
-    h2: {
-      fontSize: 20,
-      fontWeight: 'bold'
-    },
-    h5: {
-      fontSize: 20,
-      fontWeight: 'bold'
-    }
-  },
-})
-
-// function urlFor(source) {
-//   return imageUrlBuilder(client).image(source)
-// }
+import { Grid, Container, Box } from '@mui/material'
+import SimpleBlockContent from '../../SimpleBlockContent'
+import styles from './TextSeparator.module.css'
 
 function TextSeparator(props) {
-  const {portableText} = props
+  const { portableText } = props
 
-  console.log(portableText)
-
-  return ( // TODO
-    <ThemeProvider theme={theme}>
-    </ThemeProvider>
+  return (
+    <Box sx={{ backgroundColor: '#E8E8EAAD' }}>
+      <Container maxWidth="md" >
+        <Grid container>
+          <Grid item xs={12}>
+            <div className={styles.simpleBlockContent}>
+              <SimpleBlockContent blocks={portableText} />
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
