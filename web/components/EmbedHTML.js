2d8f929e064a409b02403dac34dba470367827ff
David Matos <david.matos@verity.com.br>
2022-09-15 13:49:26 -0300

    Button adjustment for not mandatory field

--- web/components/EmbedHTML.js
+++ web/components/EmbedHTML.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function EmbedHTML({node}) {
-  const {html} = node
+  const {html} = node.value
+  
   if (!html) {
     return undefined
   }
