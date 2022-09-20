5fe0d5c33954644b05360334ccc097a7790dcfdd
Ian Silva <ian.silva@verity.com.br>
2022-09-20 14:14:41 -0300

    feat: add background and center button

--- web/components/sections/TabsContent/TabsLayout/TabsLayout.js
+++ web/components/sections/TabsContent/TabsLayout/TabsLayout.js
@@ -1,56 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {createTheme, ThemeProvider} from '@mui/material/styles'
-import {Grid, Container, Box, Tabs, Tab} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { Grid, Container, Box, Tabs, Tab } from '@mui/material'
 import SimpleBlockContent from '../../../SimpleBlockContent'
 import RedirectButton from '../../../RedirectButton/RedirectButton'
 import styles from './TabsLayout.module.css'
 import NewsHorizontalLayout from '../NewsHorizontalLayout/NewsHorizontalLayout'
 import CustomPostCard from '../../custom/CustomPostCard/CustomPostCard'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import imageUrlBuilder from '@sanity/image-url'
+import client from '../../../../client'
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Europa',
-    h1: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    h5: {
-      verticalAlign: 'middle',
-      fontSize: '14px',
-      color: '#77757F',
-    },
-    p: {
-      fontSize: 16,
-      color: '#000',
-    },
-  },
-  components: {
-    MuiTab: {
-      styleOverrides: {
-        root: {
-          padding: '15px 40px',
-          fontFamily: 'Europa',
-          fontSize: '20px',
-          background: '#DC6E19',
-          color: '#fff',
-          textTransform: 'capitalize',
-          '&.Mui-selected': {
-            border: 'none',
-            color: '#fff!important',
-            textDecoration: 'underline!important',
-            textUnderlineOffset: '10px!important',
-            fontWeight: '900',
-          },
-        },
-      },
-    },
-  },
-})
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source)
+}
 
 function TabPanel(values) {
-  const {children, value, index, ...other} = values
+  const { children, value, index, ...other } = values
 
   return (
     <div
@@ -73,7 +39,7 @@ function handleTab(index) {
 }
 
 function TabsLayout(props) {
-  const {tabItems, currentLanguage} = props
+  const { tabItems, currentLanguage, backgroundImage, heading } = props
 
   const [value, setValue] = React.useState(0)
 
@@ -83,95 +49,144 @@ function TabsLayout(props) {
     setValue(newValue)
   }
 
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Europa',
+      h1: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: backgroundImage ? '#fff' : '#000',
+      },
+      h5: {
+        verticalAlign: 'middle',
+        fontSize: '14px',
+        color: backgroundImage ? '#fff' : '#77757F',
+      },
+      p: {
+        fontSize: 16,
+        color: backgroundImage ? '#fff' : '#000',
+      },
+    },
+    components: {
+      MuiTab: {
+        styleOverrides: {
+          root: {
+            padding: '15px 40px',
+            fontFamily: 'Europa',
+            fontSize: '20px',
+            background: '#DC6E19',
+            color: '#fff',
+            textTransform: 'capitalize',
+            '&.Mui-selected': {
+              border: 'none',
+              color: '#fff!important',
+              textDecoration: 'underline!important',
+              textUnderlineOffset: '10px!important',
+              fontWeight: '900',
+            },
+          },
+        },
+      },
+    },
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="lg">
-        <Grid container mb={4}>
-          <Grid item xs={12}>
-            <Box sx={{mt: -2, mb: 4, display: 'flex', justifyContent: 'center'}}>
-              <Tabs
-                orientation={mediumViewport ? "horizontal" : "vertical"}
-                value={value}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-                TabIndicatorProps={{style: {display: 'none'}}}
-              >
-                {tabItems &&
-                  tabItems.map((item, i) => {
-                    return (
-                      <Tab
-                        key={item._id}
-                        wrapped
-                        label={item.localeName[currentLanguage.languageTag] || 'Missing Tab Label'}
-                        {...handleTab(i)}
-                      />
-                    )
-                  })}
-              </Tabs>
-            </Box>
-          </Grid>
-          <Grid item xs={12} mt={5}>
-            {tabItems &&
-              tabItems.map((item, i) => {
-                const translatedButton = item.localeButton && item.localeButton[currentLanguage.languageTag];
-
-                return (
-                  <TabPanel key={item._id} value={value} index={i}>
-                    {item.newsCards?.length > 0 &&
-                      (item.isNewsCardsHorizontalLayout ? (
-                        item.newsCards?.map((item) => {
-                          return (
-                            <NewsHorizontalLayout
-                              {...item}
-                              currentLanguage={currentLanguage}
-                              key={item._id}
-                            />
-                          )
-                        })
-                      ) : (
-                        <Grid container alignItems="stretch">
-                          {item.newsCards?.map((item) => {
-                            return (
-                              <Grid item key={item._id} style={{display: 'flex'}} pt={5} md={4} pr={2}>
-                                <CustomPostCard
-                                  {...item}
-                                  languageTag={currentLanguage.languageTag}
-                                />
-                              </Grid>
-                            )
-                          })}
-                        </Grid>
-                      ))}
-                    {item.localecontentBlock && (
-                      <Grid container spacing={2} px={2}>
-                        <SimpleBlockContent
-                          blocks={item.localecontentBlock[currentLanguage.languageTag]}
+      <Box sx={{
+        background:
+          backgroundImage &&
+          `url("${urlFor(backgroundImage)
+            .url()}") no-repeat center center`,
+        backgroundSize: 'cover',
+        bgcolor: backgroundImage ? '#091b3f' : '#fff',
+      }}>
+        <Container maxWidth="lg">
+          <Grid container>
+            <Grid item xs={12}>
+              <Box sx={{ mt: -2, mb: 4, display: 'flex', justifyContent: 'center' }}>
+                <Tabs
+                  orientation={mediumViewport ? "horizontal" : "vertical"}
+                  value={value}
+                  onChange={handleChange}
+                  aria-label={`${heading} - Tab`}
+                  TabIndicatorProps={{ style: { display: 'none' } }}
+                >
+                  {tabItems &&
+                    tabItems.map((item, i) => {
+                      return (
+                        <Tab
+                          key={item._id}
+                          wrapped
+                          label={item.localeName[currentLanguage.languageTag] || 'Missing Tab Label'}
+                          {...handleTab(i)}
                         />
-                      </Grid>
-                    )}
-                    {translatedButton && (
-                      <Grid container>
-                        <Grid item xs={12} sx={{display: 'flex', justifyContent: 'center'}} my={4}>
-                          <RedirectButton
-                            {...translatedButton}
-                            sx={{
-                              padding: '10px 20px',
-                              fontSize: '16px',
-                              background: '#DC6E19',
-                              borderColor: '#DC6E19',
-                              color: '#fff',
-                              fontWeight: '300',
-                            }}
+                      )
+                    })}
+                </Tabs>
+              </Box>
+            </Grid>
+            <Grid item xs={12} mt={5}>
+              {tabItems &&
+                tabItems.map((item, i) => {
+                  const translatedButton = item.localeButton && item.localeButton[currentLanguage.languageTag]
+                  return (
+                    <TabPanel key={item._id} value={value} index={i}>
+                      {item.newsCards?.length > 0 &&
+                        (item.isNewsCardsHorizontalLayout ? (
+                          item.newsCards?.map((item) => {
+                            return (
+                              <NewsHorizontalLayout
+                                {...item}
+                                currentLanguage={currentLanguage}
+                                key={item._id}
+                              />
+                            )
+                          })
+                        ) : (
+                          <Grid container alignItems="stretch">
+                            {item.newsCards?.map((item) => {
+                              return (
+                                <Grid item key={item._id} style={{ display: 'flex' }} pt={5} md={4} pr={2}>
+                                  <CustomPostCard
+                                    {...item}
+                                    languageTag={currentLanguage.languageTag}
+                                  />
+                                </Grid>
+                              )
+                            })}
+                          </Grid>
+                        ))}
+                      {item.localecontentBlock && (
+                        <Grid container spacing={2} px={2} sx={{ background: "#fff" }}>
+                          <SimpleBlockContent
+                            blocks={item.localecontentBlock[currentLanguage.languageTag]}
                           />
                         </Grid>
-                      </Grid>
-                    )}
-                  </TabPanel>
-                )
-              })}
+                      )}
+                      {translatedButton && (
+                        <Grid container>
+                          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }} my={4}>
+                            <RedirectButton
+                              {...translatedButton}
+                              sx={{
+                                padding: '10px 20px',
+                                fontSize: '16px',
+                                background: '#DC6E19',
+                                borderColor: '#DC6E19',
+                                color: '#fff',
+                                fontWeight: '300',
+                              }}
+                            />
+                          </Grid>
+                        </Grid>
+                      )}
+                    </TabPanel>
+                  )
+                })}
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </ThemeProvider>
   )
 }
@@ -179,6 +194,8 @@ function TabsLayout(props) {
 TabsLayout.propTypes = {
   tabItems: PropTypes.object,
   currentLanguage: PropTypes.object,
+  backgroundImage: PropTypes.object,
+  heading: PropTypes.string
 }
 
 export default TabsLayout
