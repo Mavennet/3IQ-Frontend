babb7f72c6100a0ba398858919f17e6e04186341
Yousif Al-Ogaidi <haqi.yousef@gmail.com>
2022-09-21 21:24:48 +0300

    attempt to resolve merge conflict

--- web/components/sections/TabsContent/TabsLayout/TabsLayout.js
+++ web/components/sections/TabsContent/TabsLayout/TabsLayout.js
@@ -321,10 +321,12 @@ function TabsLayout(props) {
                           </Grid>
                         ))}
                       {item.localecontentBlock && (
-                        <Grid container spacing={2}>
-                          <SimpleBlockContent
-                            blocks={item.localecontentBlock[currentLanguage.languageTag]}
-                          />
+                        <Grid container spacing={2} px={2} sx={{background: '#fff'}}>
+                          <div className={styles.simpleBlockContent}>
+                            <SimpleBlockContent
+                              blocks={item.localecontentBlock[currentLanguage.languageTag]}
+                            />
+                          </div>
                         </Grid>
                       )}
                       {translatedButton && (
