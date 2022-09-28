8027b88d0127159f2ea44a18a76a43e07ff2aca6
David Matos <david.matos@verity.com.br>
2022-09-28 10:46:49 -0300

    [WEB-191] Rewriting the descriptions in order to notify the user to avoid duplicating News Cards

--- studio/schemas/documents/sections/newsCard.js
+++ studio/schemas/documents/sections/newsCard.js
@@ -10,7 +10,7 @@ export default {
       name: 'post',
       type: 'reference',
       title: 'Post (*)',
-      description: 'Select the post that this card should refer to',
+      description: "Select the post that this card should refer to. It is a good practice to keep only 1 News Card per Post, since it will avoid duplicates being displayed when enabling the News Cards Paginated Layout on a Tab Item",
       validation: Rule => Rule.error('Information required.').required(),
       to: [
         {
