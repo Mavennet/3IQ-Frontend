feb4743ac40fdd5e23e77567125dc65b1c4de114
Mahmoud Alkhraishi <mahmoud@mavennet.com>
2022-10-17 17:03:04 -0400

    useCdn to true

--- web/client.js
+++ web/client.js
@@ -3,7 +3,7 @@ import sanityClient from '@sanity/client'
 const client = sanityClient({
   projectId: 'w7ow2x7s',
   dataset: 'production',
-  useCdn: false, // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2022-01-31',
 })
 
