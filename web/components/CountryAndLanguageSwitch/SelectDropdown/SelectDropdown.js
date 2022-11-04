b41e7c711aa1b4e8c16acf91ca199055b12b9709
Yousif Al-Ogaidi <haqi.yousef@gmail.com>
2022-11-04 11:25:16 -0400

    rmv hardcode url

--- web/components/CountryAndLanguageSwitch/SelectDropdown/SelectDropdown.js
+++ web/components/CountryAndLanguageSwitch/SelectDropdown/SelectDropdown.js
@@ -59,10 +59,6 @@ function SelectDropdown(props) {
                 href={
                   country.urlTag === 'au'
                     ? 'https://3iq-au.com/'
-                    : country.urlTag === 'us'
-                    ? 'https://www.3iq-us.com/'
-                    : country.urlTag === 'ae'
-                    ? 'https://3iq.ae/'
                     : `/${country.urlTag}/home`
                 }
                 underline="hover"
