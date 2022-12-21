701af1ef7410acda52d502167f309c764f9ac8ea
Ian Silva <ian.silva@verity.com.br>
2022-12-21 15:33:49 -0300

    feat: change npm i folder

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 const client = sanityClient({
-  projectId: 'w7ow2x7s',
+  projectId: '1wgg5qqs',
   dataset: 'production',
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2022-01-31',
