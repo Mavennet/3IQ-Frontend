abe2a416fd59c83631ececd97c6e3fa65561c171
“verityjordan” <jordan.jesus@verity.com.br>
2023-01-05 13:20:41 -0300

    Fixed other pages

--- web/sections/NewLayout/TabsContent/index.js
+++ web/sections/NewLayout/TabsContent/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import TabsLayout from '../../OldLayout/TabsContent/TabsLayout'
 import AccordionLayout from './AccordionLayout'
 function TabsContent(props) {
+  const {isAccordionLayout} = props
+
+  if (isAccordionLayout) {
     return <AccordionLayout {...props} />
+  } else {
+    return <TabsLayout {...props} />
+  }
 }
 
 TabsContent.propTypes = {
